ff0b24edf7111e19f91d33ef9c6475c4951ab174
Miguel Angel EC <miguelangelec64@hotmail.com>
2024-05-03 00:53:08 -0600

    Invencibilidad y escudo implementados
    Se ha implementado la invencibilidad después de recibir daño además de al usar escudo.

--- PlaceHolderEnemy.java
+++ PlaceHolderEnemy.java
@@ -6,13 +6,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class PlaceHonderEnemy extends Enemy
+public class PlaceHolderEnemy extends Enemy
 {
     /**
      * Act - do whatever the PlaceHonderEnemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public PlaceHonderEnemy()
+    public PlaceHolderEnemy()
     {
         super(1,1,1);
     }
