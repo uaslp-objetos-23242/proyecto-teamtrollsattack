9a0ad474f05b404cf97ac909771d8068b8ea7333
JOAQUINTRUJILLO <joaquintrujilloviveros12@gmail.com>
2024-05-02 23:59:43 -0600

    Creacion de enemigos
    Se agregan las subclases de los enemigos y las imagenes correspondientes.

---
+++ MountainTroll.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MountainTroll here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MountainTroll extends Enemy
+{
+    /**
+     * Act - do whatever the MountainTroll wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public MountainTroll()
+    {
+        super(1,1,1);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
